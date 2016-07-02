470a1296aaa545d678850d8fe83687a40195f659
Kevin George Varkey <kevingeorge26@gmail.com>
2016-07-02 10:10:15 -0700

    title change

--- app/src/main/java/bunk/com/customsettings/MainActivity.java
+++ app/src/main/java/bunk/com/customsettings/MainActivity.java
@@ -3,23 +3,18 @@ package bunk.com.customsettings;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import bunk.com.customsettings.AdvancedWifi.AdvancedWifiFragment;
 import bunk.com.customsettings.Default.DefaultSettingsFragment;
-import bunk.com.customsettings.wifi.WifiFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -72,7 +67,7 @@ public class MainActivity extends AppCompatActivity
             getSupportActionBar().setTitle("Default Settings");
         } else if (id == R.id.nav_advanced_wifi) {
             fragment = AdvancedWifiFragment.newInstance();
-            getSupportActionBar().setTitle("ADV");
+            getSupportActionBar().setTitle("Configured Wifi");
         } else if (id == R.id.nav_share) {
             Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
             sharingIntent.setType("text/plain");
